010a24736a7c5e41b0d67c33f4c20ba5f9cf43da
Diliara <contact@diliaranasirova.com>
2015-01-23 00:15:09 -0800

    traversing playground

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function () {
 
+$('.vacation').on('click', 'button', function(){
+    var vacation = $(this).closest('.vacation');
+    var priceData = vacation.data('price');
+    var price = $('<p class="list-group-item-text pull-right"> $' + priceData + '</p>');
+    vacation.find('.priceArea').append(price);
+    $(this).remove();
+
+});
+
+
+});
